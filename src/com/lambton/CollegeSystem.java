55f1e4b6df1c3aa0e1f59570ca73c0b9ae96cca4
ashish <ashishpradeesh@gmail.com>
2019-05-23 19:12:16 -0400

    [Ashish] CollegeSystem added

---
+++ src/com/lambton/CollegeSystem.java
@@ -0,0 +1,14 @@
+package com.lambton;
+
+public class CollegeSystem
+{
+    public static void main (String []args)
+    {
+        System.out.println(" hello world ");
+        System.out.println(" hello Ashish ");
+
+    }
+
+
+
+}
